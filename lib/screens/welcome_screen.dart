d2e131b8f2d64a3a1ba4473d3a7dc1ed85f49934
Maybemar <maybemar.hernandez@gmail.com>
2022-05-24 23:11:01 -0500

    Refactored and added welcome screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
