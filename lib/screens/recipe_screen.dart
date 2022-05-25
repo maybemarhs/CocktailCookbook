1969888d99aad003bde72ac66812091448defe09
Maybemar <maybemar.hernandez@gmail.com>
2022-05-25 02:41:46 -0500

    Added Ingredients list in recipe screen

--- lib/screens/recipe_screen.dart
+++ lib/screens/recipe_screen.dart
@@ -9,15 +9,43 @@ class RecipeScreen extends StatefulWidget {
 }
 
 class _RecipeScreenState extends State<RecipeScreen> {
+  String getText() {
+    int i;
+    String ingredients = '';
+    String index = '';
+    for (i = 1; i < 10; i++) {
+      index = 'strIngredient$i';
+      print(index);
+      try {
+        ingredients = ingredients + '\n' + widget.drinkData['drinks'][0][index];
+      } catch (e) {
+        print(e);
+      }
+    }
+    return ingredients;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Padding(
-          padding:
-              EdgeInsets.only(left: 20.0, top: 30.0, right: 20.0, bottom: 20.0),
-          child: Text(widget.drinkData['drinks'][0]['strInstructions']),
-        ),
+            padding: EdgeInsets.only(
+                left: 20.0, top: 30.0, right: 20.0, bottom: 20.0),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20.0,
+                ),
+                Image.network(widget.drinkData['drinks'][0]['strDrinkThumb']),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text('Ingredients'),
+                Text(getText()),
+                Text(widget.drinkData['drinks'][0]['strInstructions']),
+              ],
+            )),
       ),
     );
   }
